132d1733b687214551e996e744514af2ac567b73
Muhammad Haseeb <90205212+MuhammadHaseebOdho@users.noreply.github.com>
2024-02-22 18:26:01 +0500

    Add files via upload

---
+++ MessengerApplication/src/main/java/org/webservices/resources/ProfileResource.java
@@ -0,0 +1,73 @@
+package org.webservices.resources;
+
+import java.util.List;
+
+import org.webservices.model.Profile;
+import org.webservices.service.ProfileService;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/profiles")
+public class ProfileResource {
+
+	private ProfileService profileService=new ProfileService();
+	
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Profile> getAllProfiles() {
+		return profileService.getAllProfiles();
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Profile addProfile(Profile profile) {
+		return profileService.addProfile(profile);
+	}
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{profileName}")
+	public Profile getProfile(@PathParam("profileName") String profileName) {
+		return profileService.getProfile(profileName);
+	}
+	
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/{profileName}")
+	public Profile updateProfile(@PathParam("profileName") String profileName,Profile profile) {
+		profile.setProfileName(profileName);
+		return profileService.updateProfile(profile);
+	}
+	
+	@DELETE
+	@Path("/{profileName}")
+	public void deleteProfile(@PathParam("profileName") String profileName) {
+		
+		profileService.removeProfile(profileName);
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
